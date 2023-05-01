13b7461eb85a9c15361a3114f5785fc179567f32
brunodias101 <Br@22356uno>
2023-05-01 00:07:41 -0300

    primeiro commit

---
+++ src/main/java/com/example/Autopecas/AutoPecasApplication.java
@@ -0,0 +1,13 @@
+package com.example.Autopecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoPecasApplication.class, args);
+	}
+
+}
